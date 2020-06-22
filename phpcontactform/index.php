5ea137e41f43d442a9635682fe2f09521fff97f1
Tarak HE <tarak.hentati@gmail.com>
2020-06-23 00:37:20 +0200

    Php contact form with email

---
+++ phpcontactform/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"></link>
+</head>
+<body>
+    <div>
+    <form class="contact-form" action="contact-form.php" method="post">
+        <input type="text" name="name" placeholder="Full name">
+        <input type="text" name="mail" placeholder="Your email">
+        <input type="text" name="subject" placeholder="Subject">
+        <textarea name="message" placeholder="Message"></textarea>
+        <button type="submit" name="submit">SEND MAIL</button>
+    </form>
+    </div>
+    
+</body>
+</html>
